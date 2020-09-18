5f4ff07303eba2de36981bfbe5e55f5d73013e25
acqiqi <77706579@qq.com>
2020-09-18 11:31:59 +0800

    share account

--- main.go
+++ main.go
@@ -16,10 +16,50 @@ func main() {
 	//utils.InitModel()
 
 	//查询列表
-
 	go mqtt.Init()
 	log.Println("Init Project 2020-09-05 09:18:56")
-	setup()
+	//
+	//b, err := ioutil.ReadFile("./excelout/1.jpg") // just pass the file name
+	//if err != nil {
+	//	fmt.Print(err)
+	//}
+	//
+	//list := make([]map[string]interface{}, 10)
+	//
+	//it := make(map[string]interface{})
+	//it["name"] = "name!"
+	//it["nickname"] = "nickname!"
+	//it["desc"] = b
+	//for i := 0; i < 10; i++ {
+	//	list[i] = it
+	//
+	//}
+	//
+	//keys := make([]utils.ExcelKey, 3)
+	//keys[0] = utils.ExcelKey{
+	//	Key:  "name",
+	//	Name: "Mdzz",
+	//}
+	//keys[1] = utils.ExcelKey{
+	//	Key:  "nickname",
+	//	Name: "Haha",
+	//}
+	//keys[2] = utils.ExcelKey{
+	//	Key:  "desc",
+	//	Name: "Desc",
+	//	Type: "Image",
+	//}
+	//utilsExcel := new(utils.ExcelUtils)
+	//utilsExcel.Title = "导出表格"
+	//utilsExcel.Keys = keys
+	//utilsExcel.DataBase = list
+	//utilsExcel.FilePath = "./excelout"
+	//utilsExcel.FileName = "mdzz"
+	//utilsExcel.ExcelStyle.Height = 100
+	//if err := utilsExcel.Export(); err != nil {
+	//	log.Println("导出失败", err.Error())
+	//}
+	//setup()
 	//reptile.GetDYRSNewsAll(14150,14300)
 }
 
--- utils/excel.go
+++ utils/excel.go
@@ -2,18 +2,22 @@ package utils
 
 import (
 	"github.com/360EntSecGroup-Skylar/excelize"
+	_ "image/jpeg"
+	_ "image/png"
+	"strconv"
 )
 
 type ExcelUtils struct {
-	Title     string                   `json:"title"`      //标题
-	Header    Header                   `json:"header"`     //表头
-	Keys      []ExcelKey               `json:"keys"`       //导出字段
-	DataBase  []map[string]interface{} `json:"data_base"`  //数据源
-	SaveType  string                   `json:"save_type"`  //保存类型 File Base64 UploaderUrl
-	FilePath  string                   `json:"file_path"`  //文件保存地址
-	FileName  string                   `json:"file_name"`  //表格名称
-	SheetName string                   `json:"sheet_name"` //Sheet名称
-	F         *excelize.File           `json:"f"`          //三方库实体File
+	Title      string                   `json:"title"`      //标题
+	Header     Header                   `json:"header"`     //表头
+	Keys       []ExcelKey               `json:"keys"`       //导出字段
+	DataBase   []map[string]interface{} `json:"data_base"`  //数据源
+	SaveType   string                   `json:"save_type"`  //保存类型 File Base64 UploaderUrl
+	FilePath   string                   `json:"file_path"`  //文件保存地址
+	FileName   string                   `json:"file_name"`  //表格名称
+	SheetName  string                   `json:"sheet_name"` //Sheet名称
+	F          *excelize.File           `json:"f"`          //三方库实体File
+	ExcelStyle ExcelStyle               `json:"excel_style"`
 }
 
 type Header struct {
@@ -23,9 +27,16 @@ type Header struct {
 	Height float64 `json:"height"` //高度
 }
 
+type ExcelStyle struct {
+	Style  string  `json:"style"`  //样式
+	Width  float64 `json:"width"`  //宽度
+	Height float64 `json:"height"` //高度
+}
+
 type ExcelKey struct {
 	Name string `json:"name"` //字段名
 	Key  string `json:"key"`  //字段key
+	Type string `json:"type"` //数据类型 Image
 }
 
 var abc_key = []string{
@@ -40,17 +51,19 @@ func (this *ExcelUtils) InitExcel() {
 }
 
 //导出
-func (this *ExcelUtils) Export() {
+func (this *ExcelUtils) Export() error {
 	this.InitExcel()
-	//初始化 Sheet
-	index := this.F.NewSheet(this.SheetName)
-	this.F.SetActiveSheet(index)
 	//创建表头
 	keysLen := len(this.Keys)
 	//检测Sheet名称
 	if this.SheetName == "" {
 		this.SheetName = "Sheet1"
 	}
+
+	//初始化 Sheet
+	index := this.F.NewSheet(this.SheetName)
+	this.F.SetActiveSheet(index)
+
 	//合并表头和列数量一样
 	this.F.MergeCell(this.SheetName, "A1", abc_key[keysLen]+"1")
 	this.F.SetCellValue(this.SheetName, "A1", this.Header.Title)
@@ -65,15 +78,35 @@ func (this *ExcelUtils) Export() {
 	if this.Header.Height > 0 {
 		this.F.SetRowHeight("Sheet1", 1, this.Header.Height)
 	}
-
-	//生成数据
-	for i, v := range this.DataBase {
-		for key, val := range this.Keys {
-			//插入
-			s := v[val.Key]
-		}
+	//先插入表头字段名
+	for key, value := range this.Keys {
+		this.F.SetCellValue(this.SheetName, abc_key[key+1]+"2", value.Name)
 	}
 
+	//生成数据
+	for index, item := range this.DataBase {
+		for key, val := range this.Keys {
+			//插入
+			if val.Type == "Image" {
+				this.F.AddPictureFromBytes(this.SheetName, abc_key[key+1]+strconv.Itoa(index+3),
+					`{"x_scale": 0.5, "y_scale": 0.5,"height":"10"}`,
+					"1", ".jpg",
+					item[val.Key].([]byte))
+			} else {
+				this.F.SetCellValue(this.SheetName, abc_key[key+1]+strconv.Itoa(index+3),
+					item[val.Key])
+			}
+
+			//设置表格行高
+			if this.ExcelStyle.Height > 0 {
+				this.F.SetRowHeight(this.SheetName, index+3, this.ExcelStyle.Height)
+			}
+		}
+	}
+	if err := this.F.SaveAs(this.FilePath + "/" + this.FileName + ".xlsx"); err != nil {
+		return err
+	}
+	return nil
 }
 
 //func (this *ExcelUtils)  ()  {
